864debe18ad8550d7fc41ad000607734fdb13466
CouchPugtato <dylanaulson@gmail.com>
2025-02-12 10:53:54 -0500

    updated gear ratio

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,12 +38,12 @@ public final class Constants {
 		public static final double kKrakenFreeSpeedRPM = 6000.0d;
 		public static final double kKrakenFreeSpeedRPS = kKrakenFreeSpeedRPM / 60.0d;
 		public static final double kMaxSpeed = Conversions.falconToMPS(kKrakenFreeSpeedRPS, MK4I.kWheelCircumference,
-			MK4I.kDriveGearRatio);
+			MK4I.kDriveGearRatio); // allegedly 4.7244 
 
 		public static class MK4I { // MK4i level 2s
 			public static final double kWheelRadius = Units.inchesToMeters(2.0);
 			public static final double kWheelCircumference = 2 * kWheelRadius * Math.PI; // 0.3192 meters
-			public static final double kDriveGearRatio = 425.0d / 63.0d; // TODO: Change
+			public static final double kDriveGearRatio = 6.75 / 1; // TODO: Change
 			public static final double kAngleGearRatio = 150.0d / 7.0d;
 			public static final double kCouplingRatio = 25.0d / 7.0d;
 			public static final double wheelCOF = 1.0; // default placeholder value
@@ -109,12 +109,12 @@ public final class Constants {
 	public static class Elevator {
 		// Gear ratio between the rotation of the motor and the rotation extending the elevator 
 		public static final double kRotorToSensorRatio = 1.0 / 12.0; //TODO: validate
+        public static final double kSensorToMechanismRatio = 2 * Math.PI * Units.inchesToMeters(0.8);
 		public static final double kExtensionMagnetOffset = 0.0; // TODO: find later
         public static final double kValidStateTolerance = 0.005; // meters
 		public static final double kMaxVelocity = 0; // Find desired units of setVelocity
         public static final double kMaxAcceleration = 0;
-		public static final  Constraints kMotionProfileConstraints = new Constraints(kMaxVelocity, kMaxAcceleration);
-        public static final double kSensorToMechanismRatio = 0;
+		public static final Constraints kMotionProfileConstraints = new Constraints(kMaxVelocity, kMaxAcceleration);
 	}
 
 	public static class Arm {
@@ -123,8 +123,8 @@ public final class Constants {
 		
 		public static final double kIntakeSpeed = 0.15; // 0.0-1.0
 		public static final double kValidRotationTolerance = 0.01; // In degrees TODO: Tune
-        public static final double kSensorToMechanismRatio = 0;
-        public static final double kRotorToSensorRatio = 0;
+        public static final double kSensorToMechanismRatio = 4 / 3;
+        public static final double kRotorToSensorRatio = 12 * 84 / 24; // TODO: wrong
 		
         public static final double kExtensionSafeAngle = 0;
         public static final double kPassiveCoralSpeed = 0;
@@ -152,6 +152,7 @@ public final class Constants {
 		public static final double kValidRotationTolerance = 0.01; // In degrees TODO: Tune
 		public static final double kMaxRotationalSpeed = 0;
 		public static final double kClimbPositionDegrees = 0;
+		public static final double kMinimumPositionDegrees = 0;
         public static final double kSensorToMechanismRatio = 0;
 	}
 
